5ec15010e32ae3a2fbf3b2af7abe5995ca8bed59
sadan <117494111+sadan4@users.noreply.github.com>
2024-07-20 01:27:52 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 sadan
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+
+const DONT_LOG = new Set<string>();
+const settings = definePluginSettings({
+    dontLog: {
+        type: OptionType.STRING,
+        description: "strings of loggers, broken by semicolons",
+        onChange(){
+            settings.store.dontLog?.split(";").forEach(e => DONT_LOG.add(e));
+        }
+    },
+    disableALL: {
+        description: "disables all of discord logging. \n**NOT RECCOMENDED**",
+        type: OptionType.BOOLEAN,
+        default: false
+    }
+});
+export default definePlugin({
+    authors: [{
+        id: 521819891141967883n,
+        name: "sadan"
+    }],
+    settings,
+    name: "betterConsoleJanitor",
+    description: "patches things that console janitor doesnt",
+    start(){
+        settings.store.dontLog?.split(";").forEach(e => DONT_LOG.add(e));
+    },
+    patches: [
+        {
+            find: "Σ:",
+            replacement: {
+                match: /(&&)(console)/,
+                replace: "$1$self.shouldLog(arguments[0])&&$2"
+            }
+        }
+    ],
+    shouldLog(logger: string){
+        return settings.store.disableALL ? false : !DONT_LOG.has(logger);
+    }
+});
